b7181b2099ff0efc3495dae11a8fa7689e205ec3
imsobluu <11052192+imsobluu@users.noreply.github.com>
2025-05-15 02:38:03 +0700

    Test OrderDetails on test page

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,22 +1,19 @@
 <script setup lang="ts">
 import NavBar from "@/components/navigation/NavBar.vue";
+import OrderDetails from "@/components/order/OrderDetails.vue";
 </script>
 
 <template>
 	<div class="h-full w-full flex flex-row">
-		<div class="flex flex-col flex-1">
+		<div class="flex flex-col h-full flex-2">
 			<NavBar variant="order" />
-			<div class="flex flex-1 justify-center items-center">
+			<div class="flex w-full flex-1 justify-center items-center">
 				Content
 			</div>
 		</div>
-		<div class="flex flex-col w-1/3">
-			<div class="flex h-[60px] justify-center items-center">
-				Padding
-			</div>
-			<div class="flex flex-1 justify-center items-center">
-				Details
-			</div>
-		</div>
+		<OrderDetails />
 	</div>
 </template>
+
+<style scoped>
+</style>
